432eb1c7cfc6141996314553f77ac44700c53c9f
Wagner Santiago <66319302+wagnert3@users.noreply.github.com>
2023-05-14 13:22:35 -0300

    04. Images and animations

--- teste_emulador/lib/main.dart
+++ teste_emulador/lib/main.dart
@@ -20,13 +20,16 @@ class MyApp extends StatelessWidget {
             title: Text('Tarefas'),
           ),
           body: ListView(children: [
-            Task('Aprender Flutter ssssssssssssssssssssssssssssssss ssssss'),
-            Task('Andar de Bike'),
-            Task('Meditar'),
-            Task('Meditar'),
-            Task('Meditar'),
-            Task('Meditar'),
-            Task('Meditar'),
+            Task('Aprender Flutter',
+                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large'),
+            Task('Andar de Bike',
+                'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg'),
+            Task('Meditar',
+                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg'),
+            Task('Ler',
+                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg'),
+            Task('Jogar',
+                'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg')
           ]),
           floatingActionButton: FloatingActionButton(onPressed: () {})),
     );
@@ -37,7 +40,9 @@ class MyApp extends StatelessWidget {
 
 class Task extends StatefulWidget {
   final String nome; //variavel para nome da tarefa
-  const Task(this.nome, {super.key});
+  final String foto; //variavel para foto da tarefa
+
+  const Task(this.nome, this.foto, {super.key}); // construtor
   @override
   State<Task> createState() => _TaskState();
 }
@@ -65,11 +70,11 @@ class _TaskState extends State<Task> {
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Container(
-                      color: Colors.black26,
-                      width: 72,
-                      height: 100,
-                    ),
+                    Container( // Container da foto
+                        color: Colors.black26,
+                        width: 72,
+                        height: 100,
+                        child: Image.network(widget.foto, fit: BoxFit.cover)),
                     Container(
                         // Container do texto
 
